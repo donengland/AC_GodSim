bd95c0c48203ae21a9b7386aef29e0b2acf064d9
Don England <englandpro@gmail.com>
2012-11-04 00:44:48 -0600

    Add TMX maps
    Added new tmx maps, this is a bit tricky because tmx made with tiled
    cannot import directly into andengine.  The way I fix this atm is by
    modifying the .tmx file to change the source image from "X.png" to
    "gfx/X.png"

---
+++ GodSim/assets/tmx/d4.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="d4" tilewidth="32" tileheight="32" spacing="3" margin="3">
+ <image source="../gfx/desert_tiled.png" width="281" height="211"/>
+</tileset>
