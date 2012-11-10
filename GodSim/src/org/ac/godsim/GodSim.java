ef02b1986422a02ca08e365d1935bfa9d0eb5c10
Don England <englandpro@gmail.com>
2012-11-10 03:09:31 -0700

    Add Camera Controls
    Added touch and drag camera controls, camera control handler, and
    removed some unnecessary placeholders

--- GodSim/src/org/ac/godsim/GodSim.java
+++ GodSim/src/org/ac/godsim/GodSim.java
@@ -1,6 +1,7 @@
 package org.ac.godsim;
 
 import org.andengine.engine.camera.BoundCamera;
+import org.andengine.engine.camera.SmoothCamera;
 import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
@@ -11,6 +12,7 @@ import org.andengine.entity.modifier.PathModifier;
 import org.andengine.entity.modifier.PathModifier.IPathModifierListener;
 import org.andengine.entity.modifier.PathModifier.Path;
 import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.util.FPSLogger;
@@ -22,6 +24,8 @@ import org.andengine.extension.tmx.TMXTile;
 import org.andengine.extension.tmx.TMXTileProperty;
 import org.andengine.extension.tmx.TMXTiledMap;
 import org.andengine.extension.tmx.util.exception.TMXLoadException;
+import org.andengine.input.sensor.acceleration.IAccelerationListener;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -39,24 +43,37 @@ import android.widget.Toast;
  * @author Nicolas Gramlich
  * @since 13:58:48 - 19.07.2010
  */
-public class GodSim extends SimpleBaseGameActivity {
+public class GodSim extends SimpleBaseGameActivity implements IOnSceneTouchListener{
 	// ===========================================================
 	// Constants
 	// ===========================================================
 
 	private static final int CAMERA_WIDTH = 480;
 	private static final int CAMERA_HEIGHT = 320;
+	private static final int CAMERA_X_VELOCITY = 500;
+	private static final int CAMERA_Y_VELOCITY = 500;
+	private static final int CAMERA_ZOOM_FACTOR = 500;
 
 	// ===========================================================
 	// Fields
 	// ===========================================================
 
-	private BoundCamera mBoundChaseCamera;
+	private SmoothCamera mSmoothChaseCamera;
 
 	private BitmapTextureAtlas mBitmapTextureAtlas;
 	private TiledTextureRegion mPlayerTextureRegion;
 	private TMXTiledMap mTMXTiledMap;
-	protected int mCactusCount;
+	protected int mWallCount;
+	
+	private AnimatedSprite player;
+	
+	// store touch location for X and Y in touch handlers
+	private float camTempX;
+	private float camTempY;
+	private float camTargetX = CAMERA_WIDTH;
+	private float camTargetY = CAMERA_HEIGHT;
+
+	private Scene mScene;
 
 	// ===========================================================
 	// Constructors
@@ -72,11 +89,11 @@ public class GodSim extends SimpleBaseGameActivity {
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		Toast.makeText(this, "The tile the player is walking on will be highlighted.", Toast.LENGTH_LONG).show();
+		//Toast.makeText(this, "The tile the player is walking on will be highlighted.", Toast.LENGTH_LONG).show();
 
-		this.mBoundChaseCamera = new BoundCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		this.mSmoothChaseCamera = new SmoothCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT, CAMERA_X_VELOCITY, CAMERA_Y_VELOCITY, CAMERA_ZOOM_FACTOR);
 
-		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mBoundChaseCamera);
+		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mSmoothChaseCamera);
 	}
 
 	@Override
@@ -93,15 +110,15 @@ public class GodSim extends SimpleBaseGameActivity {
 	public Scene onCreateScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 
-		final Scene scene = new Scene();
+		this.mScene = new Scene();
 
 		try {
 			final TMXLoader tmxLoader = new TMXLoader(this.getAssets(), this.mEngine.getTextureManager(), TextureOptions.BILINEAR_PREMULTIPLYALPHA, this.getVertexBufferObjectManager(), new ITMXTilePropertiesListener() {
 				@Override
 				public void onTMXTileWithPropertiesCreated(final TMXTiledMap pTMXTiledMap, final TMXLayer pTMXLayer, final TMXTile pTMXTile, final TMXProperties<TMXTileProperty> pTMXTileProperties) {
 					/* We are going to count the tiles that have the property "cactus=true" set. */
-					if(pTMXTileProperties.containsTMXProperty("cactus", "true")) {
-						GodSim.this.mCactusCount++;
+					if(pTMXTileProperties.containsTMXProperty("wall", "true")) {
+						GodSim.this.mWallCount++;
 					}
 				}
 			});
@@ -110,7 +127,7 @@ public class GodSim extends SimpleBaseGameActivity {
 			this.runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					Toast.makeText(GodSim.this, "Cactus count in this TMXTiledMap: " + GodSim.this.mCactusCount, Toast.LENGTH_LONG).show();
+					Toast.makeText(GodSim.this, "Wall count in this TMXTiledMap: " + GodSim.this.mWallCount, Toast.LENGTH_LONG).show();
 				}
 			});
 		} catch (final TMXLoadException e) {
@@ -118,63 +135,65 @@ public class GodSim extends SimpleBaseGameActivity {
 		}
 
 		final TMXLayer tmxLayer = this.mTMXTiledMap.getTMXLayers().get(0);
-		scene.attachChild(tmxLayer);
+		this.mScene.attachChild(tmxLayer);
 
 		/* Make the camera not exceed the bounds of the TMXEntity. */
-		this.mBoundChaseCamera.setBounds(0, 0, tmxLayer.getHeight(), tmxLayer.getWidth());
-		this.mBoundChaseCamera.setBoundsEnabled(true);
+		this.mSmoothChaseCamera.setBounds(0, 0, tmxLayer.getHeight(), tmxLayer.getWidth());
+		this.mSmoothChaseCamera.setBoundsEnabled(true);
 
 		/* Calculate the coordinates for the face, so its centered on the camera. */
 		final float centerX = (CAMERA_WIDTH - this.mPlayerTextureRegion.getWidth()) / 2;
 		final float centerY = (CAMERA_HEIGHT - this.mPlayerTextureRegion.getHeight()) / 2;
 
 		/* Create the sprite and add it to the scene. */
-		final AnimatedSprite player = new AnimatedSprite(centerX, centerY, this.mPlayerTextureRegion, this.getVertexBufferObjectManager());
-		this.mBoundChaseCamera.setChaseEntity(player);
+		player = new AnimatedSprite(centerX, centerY, this.mPlayerTextureRegion, this.getVertexBufferObjectManager());
+		//this.mSmoothChaseCamera.setChaseEntity(player);
+		this.mSmoothChaseCamera.setCenter(camTargetX, camTargetY);
+		player.setVisible(false);
 
-		final Path path = new Path(5).to(0, 160).to(0, 500).to(600, 500).to(600, 160).to(0, 160);
+//		final Path path = new Path(5).to(0, 160).to(0, 500).to(600, 500).to(600, 160).to(0, 160);
+//
+//		player.registerEntityModifier(new LoopEntityModifier(new PathModifier(30, path, null, new IPathModifierListener() {
+//			@Override
+//			public void onPathStarted(final PathModifier pPathModifier, final IEntity pEntity) {
+//
+//			}
+//
+//			@Override
+//			public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
+//				switch(pWaypointIndex) {
+//					case 0:
+//						player.animate(new long[]{200, 200, 200}, 6, 8, true);
+//						break;
+//					case 1:
+//						player.animate(new long[]{200, 200, 200}, 3, 5, true);
+//						break;
+//					case 2:
+//						player.animate(new long[]{200, 200, 200}, 0, 2, true);
+//						break;
+//					case 3:
+//						player.animate(new long[]{200, 200, 200}, 9, 11, true);
+//						break;
+//				}
+//			}
+//
+//			@Override
+//			public void onPathWaypointFinished(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
+//
+//			}
+//
+//			@Override
+//			public void onPathFinished(final PathModifier pPathModifier, final IEntity pEntity) {
+//
+//			}
+//		})));
+//
+//		/* Now we are going to create a rectangle that will  always highlight the tile below the feet of the pEntity. */
+//		final Rectangle currentTileRectangle = new Rectangle(0, 0, this.mTMXTiledMap.getTileWidth(), this.mTMXTiledMap.getTileHeight(), this.getVertexBufferObjectManager());
+//		currentTileRectangle.setColor(1, 0, 0, 0.25f);
+//		scene.attachChild(currentTileRectangle);
 
-		player.registerEntityModifier(new LoopEntityModifier(new PathModifier(30, path, null, new IPathModifierListener() {
-			@Override
-			public void onPathStarted(final PathModifier pPathModifier, final IEntity pEntity) {
-
-			}
-
-			@Override
-			public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
-				switch(pWaypointIndex) {
-					case 0:
-						player.animate(new long[]{200, 200, 200}, 6, 8, true);
-						break;
-					case 1:
-						player.animate(new long[]{200, 200, 200}, 3, 5, true);
-						break;
-					case 2:
-						player.animate(new long[]{200, 200, 200}, 0, 2, true);
-						break;
-					case 3:
-						player.animate(new long[]{200, 200, 200}, 9, 11, true);
-						break;
-				}
-			}
-
-			@Override
-			public void onPathWaypointFinished(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
-
-			}
-
-			@Override
-			public void onPathFinished(final PathModifier pPathModifier, final IEntity pEntity) {
-
-			}
-		})));
-
-		/* Now we are going to create a rectangle that will  always highlight the tile below the feet of the pEntity. */
-		final Rectangle currentTileRectangle = new Rectangle(0, 0, this.mTMXTiledMap.getTileWidth(), this.mTMXTiledMap.getTileHeight(), this.getVertexBufferObjectManager());
-		currentTileRectangle.setColor(1, 0, 0, 0.25f);
-		scene.attachChild(currentTileRectangle);
-
-		scene.registerUpdateHandler(new IUpdateHandler() {
+		this.mScene.registerUpdateHandler(new IUpdateHandler() {
 			@Override
 			public void reset() { }
 
@@ -186,20 +205,63 @@ public class GodSim extends SimpleBaseGameActivity {
 				/* Get the tile the feet of the player are currently waking on. */
 				final TMXTile tmxTile = tmxLayer.getTMXTileAt(playerFootCordinates[Constants.VERTEX_INDEX_X], playerFootCordinates[Constants.VERTEX_INDEX_Y]);
 				if(tmxTile != null) {
-					// tmxTile.setTextureRegion(null); <-- Rubber-style removing of tiles =D
-					currentTileRectangle.setPosition(tmxTile.getTileX(), tmxTile.getTileY());
+					tmxTile.setTextureRegion(null);// <-- Rubber-style removing of tiles =D
+					//currentTileRectangle.setPosition(tmxTile.getTileX(), tmxTile.getTileY());
 				}
 			}
 		});
-		scene.attachChild(player);
+		this.mScene.attachChild(player);
 
-		return scene;
+		this.mScene.setOnSceneTouchListener(this);
+
+		return this.mScene;
 	}
 
 	// ===========================================================
 	// Methods
 	// ===========================================================
 
+
+	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+		if(pSceneTouchEvent.isActionDown()) {
+			this.touchStart(pSceneTouchEvent.getMotionEvent().getX(), pSceneTouchEvent.getMotionEvent().getY());
+			return true;
+		}
+		if(pSceneTouchEvent.isActionMove()) {
+			this.moveCamera(pSceneTouchEvent.getMotionEvent().getX(), pSceneTouchEvent.getMotionEvent().getY());
+			return true;
+		}
+		return false;
+	}
+	private void touchStart(final float pX, final float pY){
+		System.out.println("Touched (x):" + pX + "(y):" +pY);
+		this.camTempX = pX;
+		this.camTempY = pY;
+	}
+	private void moveCamera(final float pX, final float pY){
+		System.out.println("Moved (x):" + pX + "(y):" +pY);
+
+		// Move Camera target opposite from touch direction, and reset static position
+		camTargetX += camTempX - pX;
+		camTempX = pX;		
+		camTargetY += camTempY - pY;
+		camTempY = pY;
+		
+		// checks that target is within map bounds
+		if(camTargetX < this.mSmoothChaseCamera.getBoundsXMin()){
+			camTargetX = this.mSmoothChaseCamera.getBoundsXMin();
+		}else if(camTargetX > this.mSmoothChaseCamera.getBoundsXMax()){
+			camTargetX = this.mSmoothChaseCamera.getBoundsXMax();
+		}		
+		if(camTargetY < this.mSmoothChaseCamera.getBoundsYMin()){
+			camTargetY = this.mSmoothChaseCamera.getBoundsYMin();
+		}else if(camTargetY > this.mSmoothChaseCamera.getBoundsYMax()){
+			camTargetY = this.mSmoothChaseCamera.getBoundsYMax();
+		}
+		
+		// update camera to move to new target position
+		this.mSmoothChaseCamera.setCenter(camTargetX, camTargetY);
+	}
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
